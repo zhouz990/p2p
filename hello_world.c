147d6b05fff5c0d238e0e63bfc7eddc5e1cd857c
zhouz990 <zhouz990@163.com>
2014-07-17 20:00:50 +0800

    initial project words

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello_world.c
+	> Author: ZhouZhong
+	> Mail: zhouz990@163.com 
+	> Created Time: 2014年07月17日 星期四 19时47分34秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+	printf("hello world!\n");
+	return 1;
+}
